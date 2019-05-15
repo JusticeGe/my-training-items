42889870024ca687aeec1f4b80f99c381ed32de5
Siege Lion <1091386835@qq.com>
2019-05-15 11:07:00 +0800

    [fixed](初始化):初始化算法与数据结构学习

--- js/ts/hello.ts
+++ js/ts/hello.ts
@@ -1,2 +1,7 @@
-let x: boolean = true
-console.log(x)
+function greeter(person) {
+  return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.innerHTML = greeter(user);
---
+++ leetcode/queue/code/queue/MyCircleQueue.js
@@ -0,0 +1,7 @@
+export default class MyCircleQueue{
+    constructor(k) {
+        this.list = Array(k);
+        this.front = 0;
+        this.rear = 0;
+    }
+}
