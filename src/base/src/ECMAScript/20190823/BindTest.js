c305421e1488adc2f6842f71ed5c6cbb139fb754
gechen <1091386835@qq.com>
2020-02-15 20:23:37 +0800

    🎉  初始化代码

--- src/base/src/ECMAScript/20190823/BindTest.js
+++
@@ -1,7 +0,0 @@
-let Java = {
-  name: 'Java',
-  type: '静态',
-  desc: function() {
-    console.log(`${name}是一门${type}类型语言`)
-  }
-}
--- src/front/webpack/test01/webpack.config.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const path = require('path');
-
-module.exports = {
-    entry: './src/index.js'
-}
